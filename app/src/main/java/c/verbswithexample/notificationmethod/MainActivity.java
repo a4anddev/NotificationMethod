1d4cacf417b196f01c77a7dac71b5bbb292e5d87
a4anddev <a4anddev@gmail.com>
2018-06-28 13:14:42 +0530

    Initial commit

---
+++ app/src/main/java/c/verbswithexample/notificationmethod/MainActivity.java
@@ -0,0 +1,18 @@
+package c.verbswithexample.notificationmethod;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+}
