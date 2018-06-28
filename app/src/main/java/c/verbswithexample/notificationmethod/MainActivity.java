56350a67ecada52e7833f13bb225706fb260e606
a4anddev <a4anddev@gmail.com>
2018-06-28 13:15:22 +0530

    notification method add

--- app/src/main/java/c/verbswithexample/notificationmethod/MainActivity.java
+++ app/src/main/java/c/verbswithexample/notificationmethod/MainActivity.java
@@ -2,6 +2,11 @@ package c.verbswithexample.notificationmethod;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,10 +14,44 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Resources resources;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        resources = getResources();
     }
-    
+
+    int i =0;
+    public void notifybtn(View view) {
+
+
+        Intent intent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, (int) System.currentTimeMillis(), intent, 0);
+
+
+        Bitmap bitmap = BitmapFactory.decodeResource(resources, R.mipmap.ic_launcher);
+
+        Notification notification  = new Notification.Builder(this)
+                .setContentTitle("Content title")
+                .setContentText("content Text")
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentIntent(pendingIntent)
+                .setLargeIcon(bitmap)
+                .build();
+
+        NotificationManager notificationManager = (NotificationManager)getSystemService(NOTIFICATION_SERVICE);
+        notification.flags = Notification.FLAG_AUTO_CANCEL;
+
+        notificationManager.notify(i, notification);
+
+        i++;
+
+
+
+
+
+
+    }
 }
